0b20a5a127683e33939ca24cbb18fd9cc8a3be4e
Julio Cesar Rocha <54746730+julioCROS@users.noreply.github.com>
2020-08-28 23:06:43 -0300

    Add files via upload

---
+++ 20183023405.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int num1, num2, soma;
+  printf("Digite os dois numeros: ");
+  scanf("%d %d", &num1, &num2);
+  soma = num1 + num2;
+  printf("Resultado = %d \n", soma);
+}
