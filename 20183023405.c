0a92de8c8c975c58722a98c692e306e8bc4fb986
Julio Cesar Rocha <54746730+julioCROS@users.noreply.github.com>
2020-08-28 23:08:15 -0300

    Update and rename 20183023405.c to Exercicio0.c

--- 20183023405.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int num1, num2, soma;
-  printf("Digite os dois numeros: ");
-  scanf("%d %d", &num1, &num2);
-  soma = num1 + num2;
-  printf("Resultado = %d \n", soma);
-}
---
+++ Exercicio0.c
@@ -0,0 +1,19 @@
+/*
+////////////////////////////////////////
+Exercício 0 : 
+Faça um programa que receba dois números da entrada padrão e imprima a soma deles.
+Para fazer a leitura dos números, o programa deverá imprimir na tela uma mensagem com o seguinte comando:
+printf("Digite os dois numeros: ");
+Após a leitura, o programa deverá somar os números e imprimir a resposta. A saída do programa deve seguir o modelo abaixo, considerando como entrada os números 10 e 5:
+///////////////////////////////////////
+*/
+  
+#include <stdio.h>
+
+int main() {
+  int num1, num2, soma;
+  printf("Digite os dois numeros: ");
+  scanf("%d %d", &num1, &num2);
+  soma = num1 + num2;
+  printf("Resultado = %d \n", soma);
+}
